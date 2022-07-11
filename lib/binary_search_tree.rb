2146dd76bde16bbdae5c2f4c1ae635b98de0ae7d
Darin <darinchengg@gmail.com>
2022-07-11 16:00:43 -0500

    Add Node class and include Comparable module

---
+++ lib/binary_search_tree.rb
@@ -0,0 +1,16 @@
+require 'pry-byebug'
+
+class Node
+  include Comparable
+  attr_accessor :data, :left_child, :right_child
+
+  def initialize(data = nil)
+    @data = data
+    @left_child = nil
+    @right_child = nil
+  end
+
+  def <=>(other)
+    data <=> other.data
+  end
+end
